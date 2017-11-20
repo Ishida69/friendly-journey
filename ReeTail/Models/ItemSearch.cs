25f4d17585d821911dad5bec7ad8fa5ed77575dd
billy bennett <billybennett@outlook.com>
2017-11-20 16:13:36 +0000

    Add project files.

---
+++ ReeTail/Models/ItemSearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReeTail.Models
+{
+    public class ItemSearch
+    {
+
+        [DisplayName("Item Name")]
+        [Display(Prompt = "Enter item name...")]
+        [DataType(DataType.Text)]
+        [MinLength(2), MaxLength(30)]
+        public string SearchItemName { get; set; }
+
+        [DisplayName("Filter")]
+        [Display(Prompt = "Search by filter...")]
+        [DataType(DataType.Text)]
+        [MinLength(2), MaxLength(30)]
+        public string SearchFilter { get; set; }
+
+        [DisplayName("Item Information")]
+        [DataType(DataType.Text)]   
+        [MinLength(10), MaxLength(100)]
+        public string SearchItemInformation { get; set; }   
+
+        [DisplayName("Item Availability")]
+        [DataType(DataType.Text)]
+        public string SearchItemAvailability { get; set; }
+
+        public ICollection<Items> AllItems { get; set; }
+
+    }
+}
---
+++ ReeTail/Models/UserAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReeTail.Models
+{
+    public class UserAccount
+    {
+
+        [DisplayName("Username")]
+        [Display(Prompt = "Enter username")]
+        [MinLength(1), MaxLength(30)]
+        [RegularExpression(@"[a-zA-Z\s]*$")]
+        public string Username { get; set; }
+
+        [DisplayName("Password")]
+        [Display(Prompt = "Enter password")]
+        [MinLength(1), MaxLength(30)]
+        [Required(ErrorMessage = "Please enter your password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "The email addresss is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string EmailAddress { get; set; }
+
+        [DisplayName("Postcode")]
+        [Display(Prompt = "Enter postcode")]
+        [MinLength(3), MaxLength(8)]
+        [DataType(DataType.PostalCode)]
+        public string Postcode { get; set; }
+
+        [DisplayName("Mobile Number")]
+        [Display(Prompt = "Enter your mobile number")]
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(15)]
+        public string MobileNum { get; set; }
+
+        [DisplayName("Profile Picture")]
+        [Display(Prompt = "Choose a profile picture (optional)")]
+        [DataType(DataType.ImageUrl)]
+        public string ProfileImg { get; set; }
+
+        public ICollection<Items> UserItems { get; set; }
+    }
+
+
+}
