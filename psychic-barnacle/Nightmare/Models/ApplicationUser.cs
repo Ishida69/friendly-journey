39062c983422a96dde6f3508bf5d93aef998e12f
billy bennett <billybennett@outlook.com>
2017-11-27 14:31:31 +0000

    First commit

---
+++ psychic-barnacle/Nightmare/Models/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Nightmare.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+    }
+}
---
+++ psychic-barnacle/Nightmare/Models/Item.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Nightmare.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        [Required]
+        [DisplayName("Item Name")]
+        [DataType(DataType.Text)]
+        public string ItemName { get; set; }
+        [Required]
+        [DisplayName("Item Image")]
+        [DataType(DataType.ImageUrl)]
+        public byte[] ItemImg { get; set; }
+        [Required]
+        [DisplayName("Item Description")]
+        [DataType(DataType.MultilineText)]
+        public string ItemDescription { get; set; }
+        public ApplicationUser ItemSeller { get; set; }
+        [Required]
+        [DisplayName("Item Condition")]
+        public string ItemCondition { get; set; }
+        [Required]
+        [DisplayName("Item Status")]
+        public string ItemStatus { get; set; }
+    }
+}
